c9d8c89210f784312f920ed566f5bc9636524e8b
Vitalii Abetkin <abvit89@gmail.com>
2015-02-23 18:45:16 +0300

    test

--- tests/test2.py
+++ tests/test2.py
@@ -1,22 +1,67 @@
 from time import strptime, struct_time
-from declared import Mark, DeclaredMeta
+from declared import Mark, DeclaredMeta, SkipMark
 
-class Time(Mark):
+class ParseError(Exception):
+    pass
 
-    collect_into = '_time_marks'
+class Time:
+
+    def __init__(self, hour, min):
+        self.hour = hour
+        self.min = min
+
+    @classmethod
+    def parse(cls, value):
+        try:
+            value = strptime(value, '%H:%M')
+        except ValueError:
+            raise ParseError(value)
+        return cls(value.tm_hour, value.tm_min)
+
+    def __repr__(self):
+        return '%s:%s' % (self.hour, self.min)
+
+    def __add__(self, other):
+        if isinstance(other, str):
+            try:
+                other = self.parse(other)
+            except ParseError:
+                return NotImplemented
+        elif not isinstance(other, Time):
+            return NotImplemented
+        min = self.min + other.min
+        hour, min = min // 60, min % 60
+        hour = (self.hour + other.hour + hour) % 24
+        return self.__class__(hour, min)
+
+
+class time(Mark):
+
+    collect_into = '_timepoints'
 
     def __init__(self, value):
         self.value = value
 
-    def build(self, marks, owner):
-        return strptime(self.value, '%H:%M')
+    def build(mark, marks, owner):
+        if isinstance(mark, str):
+            try:
+                return Time.parse(mark)
+            except ParseError:
+                raise SkipMark
+        return Mark.parse(mark.value)
 
+time.register(str)
 
 class DailyRoutine(metaclass=DeclaredMeta):
+    default_mark = time
 
-    breakfast = Time('9:00')
-    lunch = Time('12:00')
+class MyDailyRoutine(DailyRoutine):
+    breakfast = '9:00'
+    lunch = '12:00'
+    dinner = '-1:33' # invalid data
 
 from util import case
 
-case.assertIsInstance(DailyRoutine._time_marks['breakfast'], struct_time)
+case.assertSequenceEqual(
+    list(MyDailyRoutine._timepoints.keys()),
+    ['breakfast', 'lunch'])
